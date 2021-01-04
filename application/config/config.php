70c0a8df5dbca754a49c3055fa9dfb5787eb20fd
RyDaK <51137512+RyDaK@users.noreply.github.com>
2021-01-04 18:28:18 -0500

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://'.$_SERVER['SERVER_NAME'].'/turno';
+$config['base_url'] = 'http://'.$_SERVER['SERVER_NAME'].'/proyectoTurno';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://'.$_SERVER['SERVER_NAME'].'/turno';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = 'index.php/';
 
 /*
 |--------------------------------------------------------------------------
